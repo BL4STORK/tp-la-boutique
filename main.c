ca587eed2c9a24c4c7dd353e3e163b0048beac93
BL4STORK <144135621+BL4STORK@users.noreply.github.com>
2023-09-05 16:43:02 +0200

    V.1

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int appleprice = 10 ;
+    int sodaprice =20 ;
+    int cakeprice = 30 ;
+    int selectedprice ;
+    char product ;
+    printf("quel produit souhaitez-vous acheter\n" "si vous voulez une pomme utilisez p, si vous voulez un soda utilisez s, si vous voulez un gateau utilisez g");
+    scanf("%c", &product);
+    printf("\n") ;
+    if(product == 'p') {
+        selectedprice = appleprice ;
+    }else if (product == 's'){
+        selectedprice = sodaprice ;
+    }else if (product = 'g'){
+        selectedprice = cakeprice ;
+    }
+    selectedprice = selectedprice*1.2 ;
+    printf("Veuillez payez : %d \n",selectedprice);
+    return 0;
+}
